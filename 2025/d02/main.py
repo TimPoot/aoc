84cb334e34c509660bcb7e75b4d435e870d99992
tim <tim@localhost.localdomain>
2025-12-02 23:03:44 +0100

    Not proud of this one

--- 2025/d02/main.py
+++ 2025/d02/main.py
@@ -1,6 +1,8 @@
 import sys
 
 def part1(puzzle_input: str):
+    sum_invalid = 0
+
     with open(puzzle_input, "r") as in_file:
         for pair in in_file.readline().split(","):
 
@@ -10,16 +12,43 @@ def part1(puzzle_input: str):
                 i_str = str(i)
                 for char_idx in range(1, int(len(i_str)/2) + 1): # only check if length is equal to or smaller then length/2
                     substring = i_str[:char_idx]
-                    substring_idx = i_str.find((substring + substring), )
-                    
-                    if substring_idx != -1:
-                        print(i_str)
+                    if i_str.replace((substring + substring), "") == "":
+                        sum_invalid += i
+                        break
 
-    return "TOD"
+    return sum_invalid
 
 def part2(puzzle_input: str):
+    sum_invalid = 0
+
     with open(puzzle_input, "r") as in_file:
-        return "TODO"
+        for pair in in_file.readline().split(","):
+
+            l_bound = int(pair.split("-")[0])
+            u_bound = int(pair.split("-")[1])
+            for i in range(l_bound, u_bound + 1):
+                i_str = str(i)
+
+                for char_idx in range(1, int(len(i_str)/2) + 1): # only check if length is equal to or smaller then length/2
+                    substring = i_str[:char_idx]
+                    invalid_found = False
+
+                    for n in range(0, len(i_str)):
+                        n_times = n + 2
+                        substring_mult = substring * n_times
+
+                        if len(substring_mult) > len(i_str):
+                            break
+
+                        if i_str.replace(substring_mult, "") == "":
+                            invalid_found = True
+                            sum_invalid += i
+                            break
+
+                    if invalid_found:
+                        break
+
+    return sum_invalid
 
 if __name__ == "__main__":
     puzzle_input = sys.argv[1]
