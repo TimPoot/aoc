95c4f89742b10f58938e27aa35770297a6035e4d
tim <tim@localhost.localdomain>
2025-12-02 08:52:54 +0100

    Day 1

--- 2025/d01/main.py
+++ 2025/d01/main.py
@@ -1,12 +1,28 @@
 import sys
+import re
 
 def part1(puzzle_input: str):
-    with open(puzzle_input, "r") as in_file:
-        print(in_file)
+    return "See the excel file"
 
 def part2(puzzle_input: str):
+    regex = r"(\D)(\d+)"
+    current_pos = 50
+    zeros_passed = 0
+
     with open(puzzle_input, "r") as in_file:
-        return "TODO"
+        for line in in_file:
+            direction, distance = re.findall(regex, line)[0]
+            dir_parsed = 1 if direction == "R" else -1
+            dis_parsed = int(distance)
+
+            new_pos = current_pos + (dir_parsed * dis_parsed)
+            passed_values = range(current_pos, new_pos, dir_parsed)
+            passed_values_mod = list(map(lambda x: x % 100, passed_values))
+            zeros_passed += passed_values_mod.count(0)
+            current_pos = new_pos
+
+    return zeros_passed
+
 
 if __name__ == "__main__":
     puzzle_input = sys.argv[1]
